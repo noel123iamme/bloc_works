5c3e6a4e9be34c941443d4a232532489272949a5
Noel Deguzman <noel.deguzman@me.com>
2017-08-02 22:15:50 -0700

    adp intro

---
+++ test/bloc_works_test.rb
@@ -0,0 +1,19 @@
+require "rack/test"
+require "test-unit"
+require "bloc_works"
+
+class BlocWorksTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    BlocWorks::Application.new
+  end
+
+  def test_blocworks_application_call
+    get "/"
+    puts "last_request: #{last_request}"
+    assert_equal "Hello Blocheads!", last_response.body
+    assert last_response.ok?
+  end
+
+end
